ed5dda34fb8a101fc1082c9ae29f1962e3ee1a22
胡琤波 <657225485@qq.com>
2024-08-27 21:14:16 +0800

    go

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)$',
-  moduleNameMapper: {
-    '^@www778878net/koa78-upinfo$': path.resolve(__dirname, 'node_modules/@www778878net/koa78-upinfo/dist/src/index.js')
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
